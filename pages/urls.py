c5ae675806a64a7e22aaa87842dde356e029f09d
Jarod Jaidi <jarodjaidi@gmail.com>
2024-09-25 03:27:16 +0100

    Create ui

--- pages/urls.py
+++ pages/urls.py
@@ -5,5 +5,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
+    path('services', views.services, name='services'),  
+   
    
 ]
