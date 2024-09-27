360d060d3af090a244d108473fe5c8a2a78d025e
Jarod Jaidi <jarodjaidi@gmail.com>
2024-09-27 11:18:30 +0100

    Completed Teams Model

--- carzone/settings.py
+++ carzone/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 
 from pathlib import Path
+import psycopg2 as Database
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -32,7 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'pages.apps.PagesConfig', # Add this line
+    'pages.apps.PagesConfig', 
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -77,8 +78,11 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carzone_db',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
     }
 }
 
@@ -122,6 +126,8 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / "carzone/static",
 ]
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from .models import Team
 
 # Register your models here.
+
+class TeamAdmin(admin.ModelAdmin):
+    def thumbnail(self, object):
+        return format_html('<img src="{}" width="40" style="border-radius:50%">'.format(object.photo.url))
+    list_display = ('id','thumbnail','first_name', 'last_name', 'designation', 'created_at')
+    list_display_links = ('first_name', 'last_name')
+    search_fields = ('first_name', 'last_name', 'designation')
+    list_per_page = 10
+
+admin.site.register(Team, TeamAdmin)
+
